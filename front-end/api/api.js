3ca62600cb063dadd50c6909c8cbd48793d86765
islaianeribeiro <islaiane.lribeiro@gmail.com>
2025-02-14 21:12:01 -0300

    Novos ajustes

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import "dotenv/config";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const { NODE_ENV } = process.env;
+const URL = "https://projetospotify.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
